96073d71931cf522381d03121ed38a4656a31f77
Letargikus-oposszum <144096062+Letargikus-oposszum@users.noreply.github.com>
2023-12-13 17:23:47 +0100

    Add files via upload

---
+++ FileService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace motorok
+{
+    public class FileServices
+    {
+        public List<Data> Reading(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+
+            var allProducts = new List<Data>();
+            foreach (var line in lines.Skip(1))
+            {
+                var row = line.Replace(", ", ",");
+                var data = row.Split(',');
+
+                var newProduct = new Data();
+                newProduct.Id = int.Parse(data[0]);
+                newProduct.Name = data[1];
+                newProduct.Quantity = int.Parse(data[2]);
+                newProduct.Price = decimal.Parse(data[3].Replace(".", ","));
+                allProducts.Add(newProduct);
+            }
+            return allProducts;
+        }
+        public void FileWrite(decimal Price, int Quantity, int recordId)
+        {
+            
+            List<string> lines = File.ReadAllLines("bikes.csv").ToList();
+            for (int i = 1; i < lines.Count; i++)
+            {
+                string[] parts = lines[i].Split(',');
+
+                // Az ID megtalálása a sorban
+                if (int.TryParse(parts[0], out int id) && id == recordId)
+                {
+                    // A quantity és price frissítése
+                    parts[3] = Quantity.ToString();
+                    parts[4] = Price.ToString();
+
+                    // Az eredmény visszaírása a listába
+                    lines[i] = string.Join(",", parts);
+                    File.WriteAllLines("bikes.csv", lines);
+
+                    // Az azonosító beállítása a data objektumban
+                    var data = new Data
+                    {
+                        Id = recordId,
+                        Name = parts[1],
+                        Quantity = Quantity,
+                        Price = Price
+                    };
+
+                    // A megfelelő rekord megtalálása után nincs szükség további iterációra
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using motorok;
+using System.Diagnostics;
+
+var filePath = "Bikes.csv";
+
+var fileservice = new FileServices();
+var allProducts = fileservice.Reading(filePath);
+var bike = SelectPhone();
+var data = new Data();
+
+
+
+Data SelectPhone()
+{
+    do
+    {
+        Console.WriteLine("Melyik telefon árát szeretnénk módosítani? (Id)");
+        if (int.TryParse(Console.ReadLine(), out int productId))
+        {
+            var product = allProducts.FirstOrDefault(product => product.Id == productId);
+            if (fileservice is not null)
+                return product;
+
+            Console.WriteLine("Bike not found!");
+            continue;
+        }
+
+        Console.WriteLine("Wrong id!\r\n");
+    } while (true);
+}
+
+
+Console.WriteLine("Give me a price: ");
+int price = Convert.ToInt32(Console.ReadLine());
+if (price >= 10000000)
+{
+    Console.WriteLine("Isn't this too much for you?");
+    Environment.Exit(0);
+}
+else if (price <= 500000)
+{
+    Console.WriteLine("C'mon you really thought this is gonna be enough?");
+    Environment.Exit(0);
+}
+Console.WriteLine("Give me a quantity number: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+fileservice.FileWrite(price,quantity,bike.Id);
+
+
+Console.WriteLine($"Original data: {bike.Name}, {bike.Id}, {bike.Price}, {bike.Quantity}");
+Console.WriteLine($"The updated data: {bike.Name}, {bike.Id}, {price}, {quantity}");
+
